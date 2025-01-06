158c72d7231f4b8f29526c835adb3f6f00fcda22
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-01-06 19:52:52 -0300

    refac: change user model to delete password on return

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,21 @@ import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema({
   email: String,
+  password: String,
+});
+
+UserSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    delete ret.password; 
+    return ret;
+  },
+});
+
+UserSchema.set('toObject', {
+  transform: (doc, ret) => {
+    delete ret.password;
+    return ret;
+  },
 });
 
 export default model('User', UserSchema);
